1a6eb1faf2efabd782b26ef693347cd22db03195
Josh Pye <Josh Pye@Josh_Desktop>
2017-06-18 22:44:22 -0400

    Hey,
    I wrote this code on Friday after my shift while I was at my cottage
    where I had no internet to send this in to you. It's the GUI layout for
    my piano

---
+++ workspace/ICS3U RST/src/ICSU3U_RST_piano.java
@@ -0,0 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ICSU3U_RST_piano extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ICSU3U_RST_piano frame = new ICSU3U_RST_piano();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ICSU3U_RST_piano() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
